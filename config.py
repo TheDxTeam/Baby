71e1fd41461f2f678553f87c58122f0b5f7b1fee
DX CODER <166283380+TheDxCoderteam@users.noreply.github.com>
2024-07-04 09:48:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID",28151931))
 API_HASH = getenv("API_HASH","8dd03f69a5e342ebd65f077fb4aa97a8")
-BOT_TOKEN = getenv("BOT_TOKEN","6720481419:AAE6iITt_VGHdlF-OzUkjxD0Lva8aGIvqwI")
+BOT_TOKEN = getenv("BOT_TOKEN","6720481419:AAFvAj9E0gqW6tFNrHsacPF8yIO93YzM4Is")
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://Aryan:Aryanmusic@aryanmusic0.qzy20ty.mongodb.net/?retryWrites=true&w=majority")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1080))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002140883754 ))
